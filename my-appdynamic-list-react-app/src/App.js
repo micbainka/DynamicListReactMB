4f92b07baddc4dd13dcd1a25020ad4c4a5c5cf44
Michal <michal.bainka@bayer.com>
2025-01-03 12:26:02 +0100

    Improved Architecture

--- my-appdynamic-list-react-app/src/App.js
+++ my-appdynamic-list-react-app/src/App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import { ProductList } from "./components/ProductList/ProductList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <header> Hello world </header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 class="App-header">Welcome to the shop!</h1>
+      <ProductList sectionTitle={"Our Offer:"} />
+      
     </div>
   );
 }
---
+++ my-appdynamic-list-react-app/src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,9 @@
+export const ProductItem = (props) => {
+  return (
+  <div class="Offer-item"> 
+              <span>title: {props.title} </span>
+              <span>price: {props.price}&nbsp;EUR</span>
+  
+  
+  </div>);
+};
